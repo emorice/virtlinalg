790ff4d84fc9dcd823ae0e33bfa526724103124d
Étienne Morice <pro.emorice@mail.com>
2025-06-11 19:39:49 +0200

    test(numpy): fill in transpose test

--- tests/unit/test_numpy.py
+++ tests/unit/test_numpy.py
@@ -24,3 +24,13 @@ def test_transpose() -> None:
     """
     Can transpose matrices
     """
+    np_matrices = np.array([[0., 1.], [2., 3.]])
+
+    vla_matrices = vnp.wrap(np_matrices)
+
+    vla_results = vla_matrices.T
+
+    assert np.array_equal(
+            vnp.unwrap(vla_results),
+            np.array([[0., 2.], [1., 3.]])
+            )
