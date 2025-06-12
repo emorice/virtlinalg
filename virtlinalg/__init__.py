a8c113caaff44d4585af3f9474f221ff2a27cd70
Étienne Morice <pro.emorice@mail.com>
2025-06-12 23:09:28 +0200

    build: set up tox
    This implies:
     * switching to uv which integrates better and reorganizing dev dependencies
     * extending support to python 3.12
     * fixing misc linter warnings

--- virtlinalg/__init__.py
+++ virtlinalg/__init__.py
@@ -5,6 +5,11 @@ from .matrices import Matrices, Maps
 from .identity import identity
 from .low_rank import low_rank_product, low_rank_update
 
+__all__ = [
+        "Matrices", "Maps", "identity", "low_rank_product", "low_rank_update",
+        "inv"
+        ]
+
 def inv[M: Matrices](maps: Maps[M]) -> Maps[M]:
     """
     Attempt to invert maps
